1688271cdf03631d418247750a0ab13dc4b79611
Iain Walker <jtfjyrx@gmail.com>
2020-10-15 12:31:32 +0100

    main now works

--- main.py
+++ main.py
@@ -3,19 +3,35 @@ import sqlite3
 try:
     conn = sqlite3.connect("test.db") #create a connection and specify the database it should connect to
     #if that database doesn't exist, it creates it.
+    sqlite_create_table_query = '''CREATE TABLE table_list(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    email text NOT NULL UNIQUE);''' #prepares a 'create table' query with columns
+
     cursor = conn.cursor() #create a cursor object using the connection object returned by the connect method
     print ("Database created successfully")
+    cursor.execute(sqlite_create_table_query) #creates the table
+    conn.commit() 
+    print ("sqlite table created")
 
-    sqlite_select_Query = "Select SQLite version;"
-    cursor.execute(sqlite_select_Query) #error: No such column
-    record = cursor.fetchall()
-    print ("SQLite Database Version is: ", record) #prints SQLite database version
-    cursor.close #close the cursor opject
+    cursor.close() #close the cursor
 
 except sqlite3.Error as error: #catch database error
-    print ("error while connecting to SQLite", error) #print error
+    print ("error:", error) #print error
 
 finally:
     if (conn):
         conn.close() #close SQLite database
         print ("The SQLite connection is closed")
+
+
+############################################################
+#               CHEAT SHEET OF DATA TYPES                  #
+#----------------------------------------------------------#
+#           NULL - The Value is a null value (NULL)        #
+#        INTEGER - The Value is a numeric value (INT)      # 
+#          REAL - The Value is a float value (FLOAT)       # 
+#          TEXT - The Value is a text string (STR)         # 
+#     BLOB - The Value is a blob of data i.e. Binary       # 
+#           used to store images and files (BYTES)         #
+############################################################
