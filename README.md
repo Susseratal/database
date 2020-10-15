1f561fcff23750c343a5786f99a2ac9f90e16601
Iain Walker <jtfjyrx@gmail.com>
2020-10-15 10:58:27 +0100

    Updated README

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ B: It's designed to run on a Windows XP machine with processing power that would
 C: It's desinged to run on a Windows XP machine with no internet connection.
 
 Changelog:
-
+15/10/20:
+        Downloaded DB browser for SQLite. Seems like it'll be useful
+        Created demo file. Still doesn't work, but I'm getting there with the docs
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import sqlite3
 
-conn = sqlite3.connect("test.db")
-c = conn.cursor()
+try:
+    conn = sqlite3.connect("test.db") #create a connection and specify the database it should connect to
+    #if that database doesn't exist, it creates it.
+    cursor = conn.cursor() #create a cursor object using the connection object returned by the connect method
+    print ("Database created successfully")
 
-c.execute('''CREATE TABLE stocks
-        (date text, trans text, symbol text, qty real, price real)''')
+    sqlite_select_Query = "Select SQLite version;"
+    cursor.execute(sqlite_select_Query) #error: No such column
+    record = cursor.fetchall()
+    print ("SQLite Database Version is: ", record) #prints SQLite database version
+    cursor.close #close the cursor opject
 
-c.execute("INSERT TWO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+except sqlite3.error as error: #catch database error - Ironically error: sqlite3 has no attriubute "error"
+    print ("error while connecting to SQLite ", error) #print error
+
+finally:
+    if (conn):
+        conn.close() #close SQLite database
+        print ("The SQLite connection is closed")
